0055eb9fa2a320a792d60a54070c62f901935826
Hercules Chrysanthos <hercsma@hotmail.com>
2023-11-18 19:23:56 +0200

    Add server.js and app.js

---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const userRoute = require('./api/user/user.route');
+
+app.use(morgan('dev'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+mongoose.connect(
+  'mongodb+srv://cookhub2023:' +
+    process.env.MONGO_ATLAS_PASS +
+    '@cluster0.ihgxjrg.mongodb.net/cook-hub?retryWrites=true&w=majority'
+);
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Tpe, Accept, Authorization'
+  );
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json();
+  }
+  next();
+});
+
+app.use('/users', userRoute);
+
+app.use((req, res, next) => {
+  const error = new Error('Not Found');
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  console.log(error);
+
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
+
+module.exports = app;

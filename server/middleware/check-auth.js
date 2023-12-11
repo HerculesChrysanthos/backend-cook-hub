7162630760b8713b4a61027ad7656ceed4189550
Hercules Chrysanthos <hercsma@hotmail.com>
2023-12-12 00:35:25 +0200

    Add check auth

---
+++ server/middleware/check-auth.js
@@ -0,0 +1,31 @@
+const jwt = require('jsonwebtoken');
+
+const auth = () => {
+  return (req, res, next) => {
+    const token = req.headers?.authorization?.split(' ')[1];
+
+    try {
+      if (!token) {
+        throw new Error('A token is required for authentication');
+      }
+
+      const decoded = jwt.verify(token, process.env.TOKEN_KEY);
+      req.user = decoded;
+
+      return next();
+    } catch (error) {
+      if (error.message === 'invalid signature') {
+        error.message = 'Invalid Token';
+        error.status = 401;
+      }
+
+      if (error.message === 'A token is required for authentication') {
+        error.status = 403;
+      }
+
+      return next(error);
+    }
+  };
+};
+
+module.exports = auth;

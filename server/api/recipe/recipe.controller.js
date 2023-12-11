5fa9990ffda39e2ad90dc017160675577c55c7bb
Hercules Chrysanthos <hercsma@hotmail.com>
2023-12-12 00:35:13 +0200

    Add get my recipes functionality

--- server/api/recipe/recipe.controller.js
+++ server/api/recipe/recipe.controller.js
@@ -38,7 +38,32 @@ async function getRecipeById(req, res, next) {
   }
 }
 
+async function getMyRecipes(req, res, next) {
+  try {
+    const user = req.user.userId;
+
+    const page = req.query.page || 0;
+    const limit = req.query.limit || 9;
+
+    const recipes = await recipeService.getRecipes(page, limit, { user });
+
+    return res.status(200).json(recipes);
+  } catch (error) {
+    return next(error);
+  }
+}
+
+// async function editRecipe(req, res, next) {
+//   try {
+//     return res.status(200).json({});
+//   } catch (error) {
+//     return next(error);
+//   }
+try {
+} catch (error) {} // }
+
 module.exports = {
   getRecipes,
   getRecipeById,
+  getMyRecipes,
 };
--- server/api/recipe/recipe.router.js
+++ server/api/recipe/recipe.router.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/check-auth');
+const hasRecipeAccess = require('../../middleware/recipe-access');
 
 const recipesController = require('./recipe.controller');
 
 router.get('/', recipesController.getRecipes);
+
+router.get(
+  '/my-recipes',
+  auth(),
+  //hasRecipeAccess,
+  recipesController.getMyRecipes
+);
 
 router.get('/:recipeId', recipesController.getRecipeById);
 

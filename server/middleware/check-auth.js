d0e9a5888fdbe88710c52c7c5b0981d97a7e7c10
Hercules Chrysanthos <h.chrysanthos>
2023-12-15 18:35:45 +0200

    Add log

--- server/middleware/check-auth.js
+++ server/middleware/check-auth.js
@@ -2,8 +2,8 @@ const jwt = require('jsonwebtoken');
 
 const auth = () => {
   return (req, res, next) => {
-    console.log(req.headers);
-    console.log(req.headers?.authorization);
+    console.log('headers ', req.headers);
+    console.log('headers me auth ', req.headers?.authorization);
     const token = req.headers?.authorization?.split(' ')[1];
 
     try {

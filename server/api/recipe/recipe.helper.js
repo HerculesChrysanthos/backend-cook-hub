46b4223a8b30842cc0ff271096f7fae6cb65db04
Hercules Chrysanthos <h.chrysanthos>
2023-12-15 02:53:49 +0200

    Add resize and upload image functions

--- server/api/recipe/recipe.helper.js
+++ server/api/recipe/recipe.helper.js
@@ -1,5 +1,33 @@
-// function buildRecipesResponse(recipes){
-//     return recipes.map((recipe) => {
+const sharpHelper = require('../../helpers/sharp.helper');
+const imagekitClient = require('../../clients/imagekit-client');
 
-//     })
-// }
+function prepareImageNames(name, user) {
+  return [`${Date.now()}${user}-1${name}`, `${Date.now()}${user}-2${name}`];
+}
+
+function addImagesToRecipe(recipe, imageNames) {
+  recipe.previewImage = `https://ik.imagekit.io/cookhub/${imageNames[0]}`;
+  recipe.mainImage = `https://ik.imagekit.io/cookhub/${imageNames[1]}`;
+}
+
+function resizeAndUploadImages(buffer, names) {
+  const dimensions = [
+    { width: 150, height: 150 },
+    { width: 600, height: 400 },
+  ];
+
+  names.forEach(async (name, index) => {
+    const resizedImage = await sharpHelper.resizeImage(
+      buffer,
+      dimensions[index]
+    );
+
+    imagekitClient.uploadImage(resizedImage, name);
+  });
+}
+
+module.exports = {
+  prepareImageNames,
+  addImagesToRecipe,
+  resizeAndUploadImages,
+};

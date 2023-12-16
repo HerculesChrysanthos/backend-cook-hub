2eef4d2005e6fe3f1756d95bc06fe9b7e27a1cbd
Hercules Chrysanthos <h.chrysanthos>
2023-12-16 04:17:49 +0200

    Add getTagsByIds

--- server/api/tag/tag.repository.js
+++ server/api/tag/tag.repository.js
@@ -5,6 +5,12 @@ async function getTags() {
   return Tag.find({}).lean().exec();
 }
 
+async function getTagsByIds(ids) {
+  const objectIds = ids.map((id) => new mongoose.Types.ObjectId(id));
+  return Tag.find({ _id: { $in: objectIds } });
+}
+
 module.exports = {
   getTags,
+  getTagsByIds,
 };
--- server/api/tag/tag.service.js
+++ server/api/tag/tag.service.js
@@ -4,6 +4,11 @@ async function getTags() {
   return tagRepository.getTags();
 }
 
+async function getTagsByIds(ids) {
+  return tagRepository.getTagsByIds(ids);
+}
+
 module.exports = {
   getTags,
+  getTagsByIds,
 };

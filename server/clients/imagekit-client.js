3cc36154401371f57abb9f59f09b33f2b88cc184
Hercules Chrysanthos <h.chrysanthos>
2023-12-17 13:34:23 +0200

    Add commented await on imagekit client

--- server/clients/imagekit-client.js
+++ server/clients/imagekit-client.js
@@ -29,3 +29,42 @@ async function uploadImage(buffer, name) {
 module.exports = {
   uploadImage,
 };
+
+// const ImageKit = require('imagekit');
+// require('dotenv').config();
+
+// async function uploadImage(buffer, name) {
+//   try {
+//     const imagekit = new ImageKit({
+//       publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
+//       privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
+//       urlEndpoint: 'https://ik.imagekit.io/cookhub/',
+//     });
+
+//     const result = await new Promise((resolve, reject) => {
+//       imagekit.upload(
+//         {
+//           file: buffer,
+//           fileName: name,
+//           useUniqueFileName: false,
+//         },
+//         (error, result) => {
+//           if (error) {
+//             reject(error);
+//           } else {
+//             resolve(result);
+//           }
+//         }
+//       );
+//     });
+
+//     return result.url;
+//   } catch (error) {
+//     console.log(error);
+//     throw error;
+//   }
+// }
+
+// module.exports = {
+//   uploadImage,
+// };

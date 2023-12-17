7509d993dc7fbb111d3d755714c680f59716fc03
Hercules Chrysanthos <h.chrysanthos>
2023-12-17 13:34:39 +0200

    Add recipe access errors

--- server/middleware/recipe-access.js
+++ server/middleware/recipe-access.js
@@ -2,9 +2,17 @@ const recipeRepository = require('../api/recipe/recipe.repository');
 
 async function hasRecipeAccess(req, res, next) {
   try {
-    // const recipeId = parseInt(req.params.recipeId);
+    const recipeId = req.params.recipeId;
 
-    console.log();
+    const recipe = await recipeRepository.getRecipeById(recipeId);
+    if (!recipe) {
+      throw new Error('Recipe not found');
+    }
+
+    if (recipe.user._id !== req.user.userId) {
+      throw new Error('You have not the permission to access this');
+    }
+
     return next();
   } catch (error) {
     if (error.message === 'Recipe not found') {

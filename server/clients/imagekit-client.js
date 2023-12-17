03eedaeabf08cfcfeb969ec74564f7d0dda5b728
Hercules Chrysanthos <hercsma@hotmail.com>
2023-12-17 14:29:43 +0200

    Remove commented code

--- server/clients/imagekit-client.js
+++ server/clients/imagekit-client.js
@@ -9,18 +9,6 @@ async function uploadImage(buffer, name) {
       urlEndpoint: 'https://ik.imagekit.io/cookhub/',
     });
 
-    // imagekit.upload(
-    //   {
-    //     file: buffer,
-    //     fileName: name,
-    //     useUniqueFileName: false,
-    //   },
-    //   function (error, result) {
-    //     if (error) console.log(error);
-    //     else console.log(result);
-    //   }
-    // );
-
     return new Promise((resolve, reject) => {
       imagekit.upload(
         {
@@ -47,42 +35,3 @@ async function uploadImage(buffer, name) {
 module.exports = {
   uploadImage,
 };
-
-// const ImageKit = require('imagekit');
-// require('dotenv').config();
-
-// async function uploadImage(buffer, name) {
-//   try {
-//     const imagekit = new ImageKit({
-//       publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
-//       privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
-//       urlEndpoint: 'https://ik.imagekit.io/cookhub/',
-//     });
-
-//     const result = await new Promise((resolve, reject) => {
-//       imagekit.upload(
-//         {
-//           file: buffer,
-//           fileName: name,
-//           useUniqueFileName: false,
-//         },
-//         (error, result) => {
-//           if (error) {
-//             reject(error);
-//           } else {
-//             resolve(result);
-//           }
-//         }
-//       );
-//     });
-
-//     return result.url;
-//   } catch (error) {
-//     console.log(error);
-//     throw error;
-//   }
-// }
-
-// module.exports = {
-//   uploadImage,
-// };
